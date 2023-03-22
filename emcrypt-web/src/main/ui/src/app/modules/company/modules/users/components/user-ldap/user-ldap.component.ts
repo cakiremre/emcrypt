6ca46b96af92011ee3f910421e5e42b01d3e4a14
emre.cakir <emre@beamteknoloji.com>
2023-03-22 16:38:26 +0300

    LDAP started

--- emcrypt-web/src/main/ui/src/app/modules/company/modules/users/components/user-ldap/user-ldap.component.ts
+++ emcrypt-web/src/main/ui/src/app/modules/company/modules/users/components/user-ldap/user-ldap.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { HasSubscription } from "src/app/common/models/model";
+import { User } from "../../model/user";
+import { UserService } from "../../services/user.service";
 
 @Component({
-  selector: 'app-user-ldap',
-  templateUrl: './user-ldap.component.html',
-  styleUrls: ['./user-ldap.component.scss']
+  selector: "app-user-ldap",
+  templateUrl: "./user-ldap.component.html",
+  styleUrls: ["./user-ldap.component.scss"],
 })
-export class UserLdapComponent {
+export class UserLdapComponent
+  extends HasSubscription
+  implements OnInit, OnDestroy
+{
+  isSaving$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  isSaving: boolean;
 
+  users: Array<User> = new Array();
+  constructor(private userService: UserService) {
+    super();
+  }
+
+  loadUsersFromLdap() {}
+
+  save() {}
+
+  ngOnInit(): void {
+    // nothing here
+  }
+  ngOnDestroy(): void {
+    super.onDestroy();
+  }
 }
